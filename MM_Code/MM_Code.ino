9a1117cc80fa17cf3c357bbffcee61c0e9278a46
PieroNarciso <pieronarciso988@gmail.com>
2019-10-05 17:51:52 -0500

    Update code

--- MM_Code/MM_Code.ino
+++ MM_Code/MM_Code.ino
@@ -15,10 +15,16 @@ int Apwm = 250; //start motorA speed
 #define BIN2 10
 int Bpwm = 250; //start motorB speed
 
+// Setup start variables and arrays
+const int min_distance = 3;
+int distances[3] = {10, 10, 10}; //{left_distance, right_distance, left_distance}
+float move_vector[3] = {0, 0, 0}; // {0-1}
+
+
 void setup()
 {
   Serial.begin(9600);
-  
+
   pinMode(AIN1, OUTPUT);
   pinMode(AIN1, OUTPUT);
   pinMode(BIN1, OUTPUT);
@@ -26,30 +32,27 @@ void setup()
 
   pinMode(Apwm, OUTPUT);
   pinMode(Bpwm, OUTPUT);
-  
+
   pinMode(trig_left, OUTPUT);
   pinMode(echo_left, INPUT);
   pinMode(trig_right, OUTPUT);
   pinMode(echo_right, INPUT);
   pinMode(trig_front, OUTPUT);
   pinMode(echo_front, INPUT);
-  
 }
-
 
 void loop()
 {
   
 }
 
-
 float read_distance(int trigger, int echo)
-{/*
+{ /*
   "The function return a float distance"
     trigger: Ultrasonic's trigger pin
     echo: Ultrasonic's echo pin
   */
-  
+
   digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
@@ -65,12 +68,11 @@ void move_forward(int pin1, int pin2, int pwm, int speed_)
     pwm: Motor pwm
     speed_: Motor speed range(0, 255)
   */
-  
+
   analogWrite(pwm, speed_);
   digitalWrite(pin1, HIGH);
   digitalWrite(pin2, LOW);
 }
-
 
 void move_backward(int pin1, int pin2, int pwm, int speed_)
 { /*
@@ -80,8 +82,37 @@ void move_backward(int pin1, int pin2, int pwm, int speed_)
     pwm: Motor pwm
     speed_: Motor speed range(0, 255)
   */
-  
+
   analogWrite(pwm, speed_);
   digitalWrite(pin1, LOW);
   digitalWrite(pin2, HIGH);
+}
+
+
+
+void update_distance(int* distances)
+{
+  /*
+   "Update the distances for each sensor to a close object"
+   distances: a distance array where {left_distance, front_distance, right_distance}
+  */
+   distances[0] = read_distance(trig_left, echo_left);
+   distances[1] = read_distance(trig_front, echo_front);
+   distances[2] = read_distance(trig_right, echo_right);
+}
+
+
+void check_move(int* distances, int* move_vector, int minimun_distance)
+{
+  for(int i=0; i<3; i++)
+  {
+    if(distances[i]<=min_distance)
+    {
+      move_vector[i] = 1;
+    }
+    else
+    {
+      move_vector[i] = 0;
+    }
+  }
 }
